fce7a771802f85ecbd36fc215cc54a965b840c62
Manh hung <manhhung19960204@gmail.com>
2026-03-11 00:55:20 +0700

    khoi tao du an c ++

---
+++ Bai_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int bai10() {
+	cout << "2468 + 1234 = " << 2468 + 1234 << endl;
+    cout << "2468 - 1234 = " << 2468 - 1234 << endl;
+	cout << "2468 * 1234 = " << 2468 * 1234 << endl;
+	cout << "2468 / 1234 = " << 2468 / 1234;
+
+	return 0;
+}
+
+int main() {
+    cout << bai10();
+	return 0;
+}
