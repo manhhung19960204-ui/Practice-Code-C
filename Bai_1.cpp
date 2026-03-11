b0b788d882dd74c42f69c37218e5d084096bf4d8
Manh hung <manhhung19960204@gmail.com>
2026-03-11 21:26:13 +0700

    Practive data type

--- Bai_1.cpp
+++ Bai_1.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+int AllDataType() {
+	int x = 10;
+	string y = "Hung";
+	char z = 'K';
+	double d = 3.1555;
+	unsigned int u= 1000;
+	
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << "z = " << z << endl;
+	cout << "d = " << d << endl;
+	cout << "y = " << x ;
+
+
+}
+
 int bai10() {
 	cout << "2468 + 1234 = " << 2468 + 1234 << endl;
     cout << "2468 - 1234 = " << 2468 - 1234 << endl;
@@ -13,5 +29,6 @@ int bai10() {
 
 int main() {
     cout << bai10();
+    cout << AllDataType();
 	return 0;
 }
